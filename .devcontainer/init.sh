647c007fe7a9bdd87e1baded392f00461b394f12
Trelta Sebastion <121070310+TreltaSev@users.noreply.github.com>
2025-09-14 01:28:14 +0000

    feat(devcontainer): Lean in favor of features instead of init,sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# Install Docker
-curl -fsSL https://get.docker.com -o /tmp/scripts/get-docker.sh
-sudo sh /tmp/scripts/get-docker.sh
-
-# Install Bun because we're not losers :)
-npm i -g bun
-
-# Install Justfile
-npm i -g rust-just
-
-# Make a file so we know we're not crazy
-touch ./a.txt
-echo "aaaa" > ./a.txt
+echo "Hello there from init.sh"
