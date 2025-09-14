ca3e42ab5f6f322dd1fd3ab814c4fa23de212ee7
Trelta Sebastion <121070310+TreltaSev@users.noreply.github.com>
2025-09-14 01:09:28 +0000

    feat(init-script): Install docker aswell

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Install Justfile
-npm i -g rust-just
+# Install Docker
+curl -fsSL https://get.docker.com -o /tmp/scripts/get-docker.sh
+sudo sh /tmp/scripts/get-docker.sh
 
 # Install Bun because we're not losers :)
 npm i -g bun
+
+# Install Justfile
+bun i -g rust-just
 
 # Make a file so we know we're not crazy
 touch ./a.txt
