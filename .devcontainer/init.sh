807a5e521fea5423d0cb7033a0399ae3c7d84336
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-09-14 14:22:03 -0400

    feat(devcontainer): Change strategy

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "Hello there from init.sh"
+# Ensure bashrc
+source /.bashrc
+
+# === USER SETUP ===
+
+# Vscode user
+useradd -m -s /bin/bash -u 1000 -U vscode
+usermod -aG wheel vscode # Make user sudo
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/wheel
+
+# Docker user
+groupadd -g 998 docker || true
+usermod -aG docker vscode
+
+# copy bashrc file over
+cp /.bashrc /home/vscode/.bashrc
+
+# === PACKAGE INSTALLATION ===
+
+# bun
+curl -fsSL https://bun.com/install | bash
+
+# just
+bun i -g rust-just
+
+# ssh
+pacman -S --noconfirm openssh
